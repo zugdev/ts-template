0e77636b684476bae6abaa326cfc56d1d739e95d
zugdev <zug.dev@proton.me>
2024-11-27 01:06:20 -0300

    feat: fix errors

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,7 +9,7 @@ export default tsEslint.config({
     "@typescript-eslint": tsEslint.plugin,
     "check-file": checkFile,
   },
-  ignores: ["**/*.js"],
+  ignores: ["**/*.js", ".github/**.ts"],
   extends: [eslint.configs.recommended, ...tsEslint.configs.recommended, sonarjs.configs.recommended],
   languageOptions: {
     parser: tsEslint.parser,
