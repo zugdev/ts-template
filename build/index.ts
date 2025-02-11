169ed2c572631c57abf6b38be42c30bf4693780c
zugdev <zug.dev@proton.me>
2025-02-11 19:44:06 -0300

    test: c

--- build/index.ts
+++ build/index.ts
@@ -2,6 +2,6 @@ import * as dotenv from "dotenv";
 // load environment variables (if you have them)
 dotenv.config();
 const a = "";
-const b = "";
+const b = ""; // a
 console.log(a, b);
 console.log("Welcome to ts-template");
